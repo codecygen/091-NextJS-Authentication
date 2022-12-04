96bf8cec38a1ef3c140c9386e2ac33667dfc701b
gingercook <vahitaras@gmail.com>
2022-12-03 22:33:43 -0500

    front end will be worked on

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,8 +1,18 @@
+import { useRef } from 'react';
+
 import classes from './profile-form.module.css';
 
 function ProfileForm() {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  function passwordChangeSubmitHandler (e) {
+    e.preventDefault();
+    console.log('hi');
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={passwordChangeSubmitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
         <input type='password' id='new-password' />
